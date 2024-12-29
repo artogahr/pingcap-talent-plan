fdc71071db96f431b3dddfb9b9a0e80003ce1434
Artoghrul Gahramanli <artogahr@gmail.com>
2024-12-29 22:33:10 +0100

    Project 1 done

--- project-1/src/bin/kvs.rs
+++ project-1/src/bin/kvs.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Set { key: String, value: String },
+    Get { key: String },
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Set { key, value }) => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Some(Commands::Get { key }) => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Some(Commands::Rm { key }) => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        None => {
+            std::process::exit(1);
+        }
+    }
 }
--- project-1/src/lib.rs
+++ project-1/src/lib.rs
@@ -1,19 +1,39 @@
-pub struct KvStore {}
+//! # KvStore
+//! Simple Key Value Store
+#![deny(missing_docs)]
+use std::collections::HashMap;
+
+/// The basic implementation of the Key Value Store thingy, which uses a HashMap underneath
+/// # Examples
+/// ```
+/// use kvs::KvStore;
+/// let mut store = KvStore::new();
+/// store.set("key".to_string(), "value".to_string());
+/// ```
+pub struct KvStore {
+    storage: HashMap<String, String>,
+}
 
 impl KvStore {
+    /// Create a new Key Value Store
     pub fn new() -> KvStore {
-        KvStore {}
+        KvStore {
+            storage: HashMap::new(),
+        }
     }
 
-    pub fn set(&self, key: String, value: String) {
-        unimplemented!()
+    /// Set a key to a value
+    pub fn set(&mut self, key: String, value: String) {
+        self.storage.insert(key, value);
     }
 
+    /// Get a value ass. with a key
     pub fn get(&self, key: String) -> Option<String> {
-        unimplemented!()
+        self.storage.get(&key).cloned()
     }
 
-    pub fn remove(&self, key: String) {
-        unimplemented!();
+    /// Remove a key with it's value from the store
+    pub fn remove(&mut self, key: String) {
+        self.storage.remove(&key);
     }
 }
